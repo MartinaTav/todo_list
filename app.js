dfc6773456a2c9ca8d5a1b78f541bf5b591a7bfc
MartinaTav <libellepatisserie@gmail.com>
2020-05-06 13:42:37 +0100

    getting todo on the page

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
-console.log('it works')
+// Selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+// Event Listeners
+todoButton.addEventListener('click', addTodo);
+
+// Functions
+function addTodo(e) {
+    // prevent form from submitting
+    e.preventDefault();
+
+    // create todo div with class .todo
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+
+    // create li
+    const newTodo = document.createElement('li');
+    newTodo.classList.add('todo-item');
+    newTodo.innerText = 'hey';
+
+    // append li element to the div
+    todoDiv.appendChild(newTodo);
+
+    // button - completed
+    const completedBtn = document.createElement('button');
+    completedBtn.classList.add('complete-btn');
+    completedBtn.innerHTML = '<i class="fas fa-check"></i>';
+    todoDiv.appendChild(completedBtn);
+
+    // button - delete
+    const deleteBtn = document.createElement('button');
+    deleteBtn.classList.add('delete-btn');
+    deleteBtn.innerHTML = '<i class="fas fa-trash"></i>';
+    todoDiv.appendChild(deleteBtn);
+
+    // append to ul
+    todoList.appendChild(todoDiv);
+}
